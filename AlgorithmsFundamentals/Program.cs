b827dbee3802ac48c780a38d192872db05c47eaa
IvanSemov44 <ivan.semov.44@gmail.com>
2024-09-16 15:54:03 +0300

    02. Recursive Drawing

--- AlgorithmsFundamentals/Program.cs
+++ AlgorithmsFundamentals/Program.cs
@@ -1,5 +1,7 @@
 ﻿using RecurtionAndBackTracking;
 
+using System;
+
 namespace AlgorithmsFundamentals
 {
     // Problems Description here:  https://judge.softuni.org/Contests/Practice/DownloadResource/33715
@@ -7,11 +9,19 @@ namespace AlgorithmsFundamentals
     {
         static void Main(string[] args)
         {
+            //TODO: Make Menu for choosing one of the problems and get back in the menu
+            //TODO: Look for appropriate Design Pattern
+
             //01. Recursive Array Sum
             //RecursiveArraySum.SumRecursicely();
 
+            //02. Recursive Drawing
+            //RecursiveDrawing.DrawFigure();
+
             //4. Recursive Factorial
             //RecursiveFactorial.FactorialRecursively();
+
         }
+
     }
 }
---
+++ AlgorithmsFundamentals/RecursiveDrawing.cs
@@ -0,0 +1,49 @@
+﻿namespace RecurtionAndBackTracking
+{
+    //2. Recursive Drawing
+    //Write a program that draws the figure below depending on n.
+
+    //Exams
+    //2
+
+    //**
+    //*
+    //#
+    //##
+
+    //5
+
+    //*****
+    //****
+    //***
+    //**
+    //*
+    //#
+    //##
+    //###
+    //####
+    //#####
+
+
+    internal class RecursiveDrawing
+    {
+
+        internal static void DrawFigure()
+        {
+            int n = int.Parse(Console.ReadLine());
+            DrawFigure(n);
+        }
+
+        private static void DrawFigure(int n)
+        {
+            if (n == 0)
+                return;
+
+            Console.WriteLine(new string('*', n));
+
+            DrawFigure(n - 1);
+
+            Console.WriteLine(new string('#', n));
+        }
+    }
+}
