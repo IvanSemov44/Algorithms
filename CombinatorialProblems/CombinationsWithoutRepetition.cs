5f4726b32e268e6db94461dca1e4c920512ad1de
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-27 13:58:35 +0200

    Combinations without Repetition

---
+++ CombinatorialProblems/CombinationsWithoutRepetition.cs
@@ -0,0 +1,49 @@
+﻿namespace CombinatorialProblems
+{
+    //    5. Combinations without Repetition
+    //Given a set of elements, generate all combinations of k elements without repetition.
+
+    //Examples
+
+    //Input
+    //A B C
+    //2
+
+    //Output
+    //A B
+    //A C
+    //B C
+
+    internal class CombinationsWithoutRepetition
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+
+        public static void Solution()
+        {
+            elements = new[] { "A", "B", "C" };
+            k = 2;
+            //elements = Console.ReadLine().Split();
+            //k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            Combinations(0, 0);
+        }
+        private static void Combinations(int idx, int elementsStartIdx)
+        {
+            if (idx >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = elementsStartIdx; i < elements.Length; i++)
+            {
+                variations[idx] = elements[i];
+                Combinations(idx + 1, i + 1);
+            }
+        }
+    }
+}
--- CombinatorialProblems/Program.cs
+++ CombinatorialProblems/Program.cs
@@ -1,15 +1,20 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace CombinatorialProblems
 {
     //Problems Description here: https://judge.softuni.org/Contests/Practice/DownloadResource/33716
     internal class Program
     {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+
         static void Main(string[] args)
         {
-            //1.Permutations without Repetitions
+            //1. Permutations without Repetitions
             //PermutationsWithoutRepetition.Solution();
             //PermutationsWithoutRepetitionWithUsingExtraMemory.Solution();
 
@@ -19,9 +24,36 @@ namespace CombinatorialProblems
             //3. Variations without Repetitions
             //VariationsWithoutRepetition.Solution();
 
-            VariationsWithRepetition.Solution();
+            //4. Variations with Repetition
+            //VariationsWithRepetition.Solution();
 
+            //elements = new[] { "A", "B", "C" };
+            //k = 2;
 
+            //5. Combinations without Repetition
+            //CombinationsWithoutRepetition.Solution();
+
+            //elements = Console.ReadLine().Split();
+            //k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            Combinations(0, 0);
+
+        }
+        private static void Combinations(int idx, int elementsStartIdx)
+        {
+            if (idx >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = elementsStartIdx; i < elements.Length; i++)
+            {
+                variations[idx] = elements[i];
+                Combinations(idx + 1, i + 1);
+            }
         }
 
     }
