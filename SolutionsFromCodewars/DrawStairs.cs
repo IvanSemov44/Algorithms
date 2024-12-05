6d42878af1ead25e3d356d70639a1e724d4b4d4a
IvanSemov44 <ivan.semov.44@gmail.com>
2024-12-05 13:54:06 +0200

    Draw stairs

---
+++ SolutionsFromCodewars/DrawStairs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionsFromCodewars
+{
+    //https://www.codewars.com/kata/5b4e779c578c6a898e0005c5/
+    internal class DrawStairs
+    {
+        public static void Solution()
+        {
+            int n = 7;
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    result.Append(" ");
+                }
+                if (i == n - 1)
+                    result.Append("I");
+                else
+                    result.Append("I\n");
+            }
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ SolutionsFromCodewars/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SolutionsFromCodewars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
