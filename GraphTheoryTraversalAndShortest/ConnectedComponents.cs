d09f7e66a8b39ffcd64f860dde794c42aafeed35
IvanSemov44 <ivan.semov.44@gmail.com>
2024-12-19 15:18:20 +0200

    ConnectedComponents

---
+++ GraphTheoryTraversalAndShortest/ConnectedComponents.cs
@@ -0,0 +1,65 @@
+﻿namespace GraphTheoryTraversalAndShortest
+{
+    //https://judge.softuni.org/Contests/Practice/DownloadResource/33719
+
+    /*
+      1. Connected Components
+    The first part of this lab aims to implement the DFS algorithm (Depth-First-Search) to traverse a graph and find its
+    connected components (nodes connected either directly, or through other nodes). The graph nodes are numbered
+    from 0 to n-1. The graph comes from the console in the following format:
+        • First line: number of lines n
+        • Next n lines: list of child nodes for the nodes 0 … n-1 (separated by a space)
+     */
+
+    internal class ConnectedComponents
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+
+        public static void Solution()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[n];
+            visited = new bool[n];
+
+            for (int node = 0; node < n; node++)
+            {
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(line))
+                    graph[node] = new List<int>();
+                else
+                {
+                    var children = line.Split()
+                        .Select(int.Parse)
+                        .ToList();
+
+                    graph[node] = children;
+                }
+            }
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (visited[node]) continue;
+
+                var component = new List<int>();
+                DSF(node, component);
+
+                Console.WriteLine($"Connected component: {string.Join(" ", component)}");
+            }
+        }
+
+        private static void DSF(int node, List<int> component)
+        {
+            if (visited[node]) return;
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+                DSF(child, component);
+
+            component.Add(node);
+        }
+    }
+}
--- GraphTheoryTraversalAndShortest/Program.cs
+++ GraphTheoryTraversalAndShortest/Program.cs
@@ -1,60 +1,16 @@
-﻿
-using System.Net.Quic;
-using System.Xml.Linq;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace GraphTheoryTraversalAndShortest
 {
     internal class Program
     {
-        private static Dictionary<int, List<int>> graph;
-        private static HashSet<int> visited;
-
         static void Main(string[] args)
         {
-            graph = new Dictionary<int, List<int>>()
-            {
-                { 1, new List<int>()  { 19, 21, 14 } },
-                { 19, new List<int>() { 7, 12, 31, 23 } },
-                { 7, new List<int>()  { 1 } },
-                { 31, new List<int>() { 21 } },
-                { 21, new List<int>() { 14 } },
-                { 23, new List<int>() { 21 } },
-                { 14, new List<int>() { 23, 6} },
-                { 12, new List<int>() },
-                { 6, new List<int>() },
-            };
-
-            visited = new HashSet<int>();
-
-            foreach (var node in graph.Keys)
-                BFS(node);
+            ConnectedComponents.Solution();
         }
 
-        private static void BFS(int startNode)
-        {
-            if (visited.Contains(startNode)) return;
 
-            var queue = new Queue<int>();
-
-            queue.Enqueue(startNode);
-            visited.Add(startNode);
-
-            while (queue.Count > 0)
-            {
-                var node = queue.Dequeue();
-
-                Console.WriteLine(node);
-
-                foreach (var child in graph[node])
-                {
-                    if (!visited.Contains(child))
-                    {
-                        visited.Add(child);
-                        queue.Enqueue(child);
-                    }
-                }
-
-            }
-        }
     }
 }
