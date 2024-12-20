ef0700a584cab778d267047094957d29a253f28c
IvanSemov44 <ivan.semov.44@gmail.com>
2024-12-20 15:49:02 +0200

    Topological Sorting With DFS

---
+++ GraphTheoryTraversalAndShortest/TopologicalSortingWithDFS.cs
@@ -0,0 +1,82 @@
+﻿namespace GraphTheoryTraversalAndShortest
+{
+    //https://judge.softuni.org/Contests/Practice/DownloadResource/33719
+    //2. DFS Topological Sorting
+
+    internal static  class TopologicalSortingWithDFS
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static Stack<string> sorted;
+        private static HashSet<string> visited;
+        private static HashSet<string> cycles;
+
+        public static void Solution()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(n);
+            sorted = new Stack<string>();
+            visited = new HashSet<string>();
+            cycles = new HashSet<string>();
+
+            var hasCycle = false;
+
+            foreach (var node in graph.Keys)
+                try
+                {
+                    DFS(node);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    hasCycle = true;
+                    break;
+                }
+
+            if (!hasCycle)
+                Console.WriteLine("Topological sorting:" + string.Join(" ", sorted));
+        }
+
+        private static void DFS(string node)
+        {
+            if (cycles.Contains(node))
+                throw new InvalidOperationException("The given graph is cyclic.");
+
+            if (visited.Contains(node)) return;
+
+            cycles.Add(node);
+            visited.Add(node);
+
+            foreach (var child in graph[node])
+                DFS(child);
+
+            sorted.Push(node);
+            cycles.Remove(node);
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var parts = Console.ReadLine()
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(e => e.Trim())
+                    .ToArray();
+
+                var key = parts[0];
+
+                if (parts.Length == 1)
+                    result[key] = new List<string>();
+                else
+                {
+                    var children = parts[1].Split(", ").ToList();
+                    result[key] = children;
+                }
+            }
+
+            return result;
+        }
+    }
+}
