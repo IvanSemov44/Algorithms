2844dbf8698a050797f8d6092856012650f0c176
IvanSemov44 <ivan.semov.44@gmail.com>
2024-12-21 14:59:39 +0200

    Shortest Path

--- GraphTheoryTraversalAndShortest/Program.cs
+++ GraphTheoryTraversalAndShortest/Program.cs
@@ -6,18 +6,10 @@ namespace GraphTheoryTraversalAndShortest
 {
     internal class Program
     {
-        private static Dictionary<string, List<string>> graph;
-        private static Stack<string> sorted;
-        private static HashSet<string> visited;
-        private static HashSet<string> cycles;
-
         static void Main(string[] args)
         {
-            //ConnectedComponents.Solution();
-
-            
+           
         }
 
-       
     }
 }
