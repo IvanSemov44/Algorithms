dbca283a4d504cc1a3b8ee69514db1e5f291211e
IvanSemov44 <ivan.semov.44@gmail.com>
2025-01-05 23:56:27 +0200

    Introduction To Dynamic Programming Project Add and Fibonacci solution

---
+++ IntroductionToDynamicProgramming/Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿namespace IntroductionToDynamicProgramming
+{
+    /*
+    1. Fibonacci
+    Write a dynamic programming solution for finding nth Fibonacci members.
+    • F0 = 0
+    • F1 = 1
+     */
+    //Input Output
+    /*
+    20 ---- 6765
+    50 ---- 12586269025
+    */
+
+    internal static class Fibonacci
+    {
+        private static Dictionary<int, long> cache = new Dictionary<int, long>();
+
+        public static void Solution()
+        {
+
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalcFib(n));
+        }
+
+        private static long CalcFib(int n)
+        {
+            if (cache.ContainsKey(n))
+                return cache[n];
+
+            if (n < 2)
+                return n;
+
+            var result = CalcFib(n - 1) + CalcFib(n - 2);
+            cache[n] = result;
+
+            return CalcFib(n - 1) + CalcFib(n - 2);
+        }
+    }
+}
---
+++ IntroductionToDynamicProgramming/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace IntroductionToDynamicProgramming
+{
+    //Problems Description here: https://judge.softuni.org/Contests/Practice/DownloadResource/33721
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Fibonacci
+            //Fibonacci.Solution();
+        }
+    }
+}
