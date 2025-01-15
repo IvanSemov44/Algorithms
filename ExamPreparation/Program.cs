cdea1b51d0d0cbb4fcba724af0cf1a673e5e32a6
IvanSemov44 <ivan.semov.44@gmail.com>
2025-01-15 15:45:27 +0200

    Train

---
+++ ExamPreparation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPreparation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //01. Trains https://judge.softuni.org/Contests/Practice/DownloadResource/23684
+            Train.Solution();
+        }
+    }
+}
---
+++ ExamPreparation/Train.cs
@@ -0,0 +1,51 @@
+﻿
+namespace ExamPreparation
+{
+    internal static class Train
+    {
+        public static void Solution()
+        {
+            var arrival = Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .OrderBy(x => x)
+            .ToArray();
+
+            var departure = Console.ReadLine()
+              .Split()
+              .Select(double.Parse)
+              .OrderBy(x => x)
+              .ToArray();
+
+            Console.WriteLine(FindMinPlatforms(arrival, departure));
+        }
+        private static int FindMinPlatforms(double[] arrival, double[] departure)
+        {
+            var totalPlatforms = 0;
+            var platforms = 0;
+
+            var arrivalIdx = 0;
+            var departureIdx = 0;
+
+            while (arrivalIdx < arrival.Length)
+            {
+                var arrivalTime = arrival[arrivalIdx];
+                var departureTime = departure[departureIdx];
+
+                if (arrivalTime < departureTime)
+                {
+                    platforms++;
+                    arrivalIdx++;
+                    totalPlatforms = Math.Max(platforms, totalPlatforms);
+                }
+                else
+                {
+                    departureIdx++;
+                    platforms--;
+                }
+            }
+
+            return totalPlatforms;
+        }
+    }
+}
