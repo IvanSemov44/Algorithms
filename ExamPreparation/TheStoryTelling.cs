3d03d1170e1cf8cccc28fa17c3859d4e0aac2a39
IvanSemov44 <ivan.semov.44@gmail.com>
2025-01-21 14:10:45 +0200

    improven Two Minutes to Midnight solution

---
+++ ExamPreparation/TheStoryTelling.cs
@@ -0,0 +1,89 @@
+﻿namespace ExamPreparation
+{
+    internal  static class TheStoryTelling
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static Dictionary<string, int> dependencies;
+
+        public static void Solution()
+        {
+            graph = ReadGraph();
+            dependencies = ExtractDependencies(graph);
+            var sorted = TopologicalSorted(dependencies);
+
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+
+        private static List<string> TopologicalSorted(Dictionary<string, int> dependencies)
+        {
+            var sorted = new List<string>();
+
+            while (dependencies.Count > 0)
+            {
+                var nodeToRemove = dependencies.FirstOrDefault(x => x.Value == 0).Key;
+
+                if (nodeToRemove == null)
+                    break;
+
+                dependencies.Remove(nodeToRemove);
+                sorted.Add(nodeToRemove);
+
+                foreach (var child in graph[nodeToRemove])
+                    dependencies[child] -= 1;
+            }
+
+            return sorted;
+        }
+
+        private static Dictionary<string, int> ExtractDependencies(Dictionary<string, List<string>> graph)
+        {
+            var result = new Dictionary<string, int>();
+
+            foreach (var kvp in graph)
+            {
+                var node = kvp.Key;
+                var children = kvp.Value;
+
+                if (!result.ContainsKey(node))
+                    result[node] = 0;
+
+                foreach (var child in children)
+                    if (!result.ContainsKey(child))
+                        result[child] = 1;
+                    else
+                        result[child]++;
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph()
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                var parts = Console.ReadLine()
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                var key = parts[0];
+
+                if (key == "End") break;
+
+                if (parts.Length == 1)
+                {
+                    result[key] = new List<string>();
+                }
+                else
+                {
+                    var children = parts[1].Split().ToList();
+                    result[key] = children;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ExamPreparation/Time.cs
@@ -0,0 +1,56 @@
+﻿namespace ExamPreparation
+{
+    internal static class Time
+    {
+        private static int[][] lcs;
+        public static void Solution()
+        {
+            var first = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var second = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            InitializeTable(first, second);
+
+            Stack<int> longestCommonSubsequence = new Stack<int>();
+
+            int row = first.Length;
+            int col = second.Length;
+
+            while (row > 0 && col > 0)
+                if (first[row - 1] == second[col - 1])
+                {
+                    longestCommonSubsequence.Push(first[row - 1]);
+                    row--;
+                    col--;
+                }
+                else if (lcs[row][col - 1] >= lcs[row - 1][col])
+                    col--;
+                else
+                    row--;
+
+            Console.WriteLine(string.Join(" ", longestCommonSubsequence));
+            Console.WriteLine(lcs[^1][^1]);
+        }
+
+        private static void InitializeTable(int[] firstSequence, int[] secondSequence)
+        {
+            lcs = new int[firstSequence.Length + 1][];
+
+            for (int row = 0; row < lcs.Length; row++)
+                lcs[row] = new int[secondSequence.Length + 1];
+
+            for (int row = 1; row < lcs.Length; row++)
+                for (int col = 1; col < lcs[row].Length; col++)
+                    if (firstSequence[row - 1] == secondSequence[col - 1])
+                        lcs[row][col] = lcs[row - 1][col - 1] + 1;
+                    else
+                        lcs[row][col] = Math.Max(lcs[row][col - 1], lcs[row - 1][col]);
+        }
+    }
+}
