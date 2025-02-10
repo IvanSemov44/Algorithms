cd713fbf62069bd618c5764ed01948208763bb2b
IvanSemov44 <ivan.semov.44@gmail.com>
2025-02-10 14:18:37 +0200

    MostReliablePath

---
+++ ExerciseGraphsBellmanFordLongestPathInDAG/Edge.cs
@@ -0,0 +1,9 @@
+﻿namespace ExerciseGraphsBellmanFordLongestPathInDAG
+{
+    internal class Edge
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ ExerciseGraphsBellmanFordLongestPathInDAG/Program.cs
@@ -0,0 +1,22 @@
+﻿using Wintellect.PowerCollections;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExerciseGraphsBellmanFordLongestPathInDAG
+{
+    //https://judge.softuni.org/Contests/Practice/DownloadResource/36965
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MostReliablePath.Solution();
+        }
+    }
+    internal class Edge1
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+}
