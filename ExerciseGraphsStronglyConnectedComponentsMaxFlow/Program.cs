9f0aff95a003fd9ca6d29a11386527241745c8ab
IvanSemov44 <ivan.semov.44@gmail.com>
2025-02-16 17:05:31 +0200

    Recontruction and Rename the project

---
+++ ExerciseGraphsStronglyConnectedComponentsMaxFlow/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciseGraphsStronglyConnectedComponentsMaxFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
