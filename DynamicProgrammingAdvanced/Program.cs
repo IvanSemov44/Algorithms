3d8340a96fcfc8dc8342a53cfcd2972489727c0f
IvanSemov44 <ivan.semov.44@gmail.com>
2025-02-24 13:46:32 +0200

    Rod Cutting

---
+++ DynamicProgrammingAdvanced/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DynamicProgrammingAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://judge.softuni.org/Contests/Practice/DownloadResource/36968
+            //RodCutting.Solution();
+        }
+    }
+}
---
+++ DynamicProgrammingAdvanced/RodCutting.cs
@@ -0,0 +1,64 @@
+﻿namespace DynamicProgrammingAdvanced
+{
+    internal static class RodCutting
+    {
+        private static int[] memo;
+        private static int[] prev;
+
+        public static void Solution()
+        {
+            var price = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            memo = new int[price.Length];
+            prev = new int[price.Length];
+
+            var length = int.Parse(Console.ReadLine());
+
+            var bestPrice = CutRod(price, length);
+
+            Console.WriteLine(bestPrice);
+
+            var parts = new List<int>();
+
+            while (length != 0)
+            {
+                var currentPrev = prev[length];
+                parts.Add(currentPrev);
+                length -= currentPrev;
+            }
+
+            Console.WriteLine(string.Join(" ", parts));
+        }
+
+        private static int CutRod(int[] price, int length)
+        {
+            if (length == 0)
+                return 0;
+
+            if (memo[length] != 0)
+                return price[length];
+
+            var bestPrice = price[length];
+            var bestCombo = length;
+
+            for (var i = 1; i < length; i++)
+            {
+                var currentPrice = price[i] + CutRod(price, length - i);
+
+                if (currentPrice > bestPrice)
+                {
+                    bestPrice = currentPrice;
+                    bestCombo = i;
+                }
+            }
+
+            memo[length] = bestPrice;
+            prev[length] = bestCombo;
+
+            return bestPrice;
+        }
+    }
+}
