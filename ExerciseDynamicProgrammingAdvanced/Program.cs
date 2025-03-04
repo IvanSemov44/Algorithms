c0548b0f3f692ae1720c2800b7af376cccad8fb8
IvanSemov44 <ivan.semov.44@gmail.com>
2025-03-04 13:33:35 +0200

    Battle Points

--- ExerciseDynamicProgrammingAdvanced/Program.cs
+++ ExerciseDynamicProgrammingAdvanced/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
+            //Assignment
             //https://softuni.bg/downloads/svn/Algorithms-with-Csharp/Algorithms-Advanced/Jul-2023/07.%20Exercise-Dynamic-Programming-Advanced/07.%20Exercise-Dynamic-Programming-Advanced.pdf
-            CableMerchant.Solution();
+
+            //CableMerchant.Solution();
+
+            //BattlePoints.Solution();
         }
     }
 }
