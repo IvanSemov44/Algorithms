b2dc5bfa8892540cbc12d5d7fa75caab5bf8630c
IvanSemov44 <ivan.semov.44@gmail.com>
2025-07-24 16:19:15 +0300

    HighestAndLowest

---
+++ SolutionsFromCodewars/HighestAndLowest.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace SolutionsFromCodewars
+{
+    public static class HighestAndLowest
+    {
+        public static string HighAndLow(string number)
+        {
+            var numbers = number.Split(' ').Select(int.Parse).ToArray();
+
+            var min = int.MaxValue;
+            var max = int.MinValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                    min = numbers[i];
+
+                if (numbers[i] > max)
+                    max = numbers[i];
+            }
+
+            return max + " " + min;
+        }
+    }
+}
